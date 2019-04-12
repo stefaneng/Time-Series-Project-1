abf637c456786fa8c031e88725f77cc1df5e96aa
Stefan Eng <stefaneng13@gmail.com>
2019-04-12 10:45:17 +0200

    Implement ljungbox

---
+++ ljungbox.m
@@ -0,0 +1,13 @@
+function lambda = ljungbox(data, h)
+    % Assumes that data is mean centered
+    gamma = 1:h+1;
+    n = length(data);
+   
+    for i = 0:h
+        gamma(i + 1) = data(1 + i:end)' * data(1:end - i) / n;
+    end
+    
+    acf = gamma(2:end) / gamma(1);
+    
+    lambda = n * (n + 2) * sum(acf.^2 / (n - 1));
+end
--- project1.m
+++ project1.m
@@ -7,22 +7,24 @@ x_t_1 = data(2:end);
 
 abs_returns = x_t_1 - x_t;
 log_returns = log(x_t_1) - log(x_t);
+
 n_returns = length(abs_returns);
 
-corr_abs_returns = abs_returns - mean(abs_returns);
-corr_log_returns = log_returns - mean(log_returns);
+corrected_abs_returns = abs_returns - mean(abs_returns);
+corrected_log_returns = log_returns - mean(log_returns);
+corrected_data = data - mean(data);
 
 figure;
-subplot(1,2,1);
-plot(corr_abs_returns);
-subplot(1,2,2);
-plot(corr_log_returns);
+subplot(2,2,1);
+title("Exchange data");
+plot(corrected_data);
+subplot(2,2,2);
+title("Absolute returns");
+plot(corrected_abs_returns);
+subplot(2,2,3);
+title("Log Returns");
+plot(corrected_log_returns);
 
 % Problem 2
-% Incorrect, TODO
-h = 20;
-gamma0 = (data - mean(data))' * (data - mean(data)) / n_data;
-gamma20 = data(1 + h:end) - mean(data)' * (data(1:end - h) - mean(data)) / n_data;
-acf = gamma20 / gamma0;
-
+ljungbox(corrected_data,20);
 
