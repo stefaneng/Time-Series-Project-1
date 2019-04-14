6483d6fa943d0ea5139bd939c03673db1a4e7abc
Stefan Eng <stefaneng13@gmail.com>
2019-04-14 12:14:20 +0200

    Add predictions for problem 3

--- project1.m
+++ project1.m
@@ -16,14 +16,14 @@ corrected_data = data - mean(data);
 
 figure;
 subplot(2,2,1);
-title("Exchange data");
 plot(corrected_data);
+title("Exchange data");
 subplot(2,2,2);
-title("Absolute returns");
 plot(corrected_abs_returns);
+title("Absolute returns");
 subplot(2,2,3);
-title("Log Returns");
 plot(corrected_log_returns);
+title("Log Returns");
 
 % Problem 2
 crit_val = chi2inv(0.95, 20);
@@ -36,17 +36,30 @@ lambda_data > crit_val;
 lambda_abs > crit_val;
 lambda_log > crit_val;
 
-% TODO: Plot on same scale
 % IID should be 0 covariance
 figure;
 subplot(2,2,1);
-plot(acf_data);
+stem(acf_data, 'filled');
+% Draw +- 1.96 / sqrt(n) lines
+yline(1.96 / sqrt(n_data), '--', '1.96/sqrt(n)');
+yline(-1.96 / sqrt(n_data), '--', '-1.96/sqrt(n)');
+title("ACF for original data");
 axis([1 20 -1 1]);
+
 subplot(2,2,2);
-plot(acf_abs);
+stem(acf_abs, 'filled');
+% Draw +- 1.96 / sqrt(n) lines
+yline(1.96 / sqrt(n_returns), '--', '1.96/sqrt(n)');
+yline(-1.96 / sqrt(n_returns), '--', '-1.96/sqrt(n)');
+title("ACF for absolute returns");
 axis([1 20 -1 1]);
+
 subplot(2,2,3);
-plot(acf_log);
+stem(acf_log, 'filled');
+% Draw +- 1.96 / sqrt(n) lines
+yline(1.96 / sqrt(n_returns), '--', '1.96/sqrt(n)');
+yline(-1.96 / sqrt(n_returns), '--', '-1.96/sqrt(n)');
+title("ACF for log returns");
 axis([1 20 -1 1]);
 
 % Problem 3
@@ -54,8 +67,27 @@ training = corrected_log_returns(1:102);
 test = corrected_log_returns(103:end);
 [gm, train_gamma_mat, train_acf] = acvf(training, 20);
 
+% First coefficient, a_0 is zero since mean is zero.
+coefs = train_gamma_mat \ flip(gm(2:end)');
+
+preds = zeros(n_returns, 1);
+preds(1:102) = training;
+for i = 103:n_returns
+    preds(i) = dot(preds(i-20:i-1), coefs);
+end
+
+figure;
+
+% TODO: Plot the predictions and the actual values.
+scatter(103:n_returns, preds(103:n_returns), 'black');
+scatter(103:n_returns, test, [], 'red');
+
+% mean squared error
+mean(preds(103:n_returns) - test)
+
 % Problem 4
 % qqplot(corrected_log_returns);
+figure;
 x = corrected_log_returns / std(corrected_log_returns);
 cdfplot(x)
 hold on;
