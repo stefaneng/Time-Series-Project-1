9d81a674f408f3ff2716bbd3dd9083de35f9f93d
Stefan Eng <stefaneng13@gmail.com>
2019-04-14 07:36:21 +0200

    Refactor acvf into own function

--- ljungbox.m
+++ ljungbox.m
@@ -1,11 +1,6 @@
 function [lambda, acf] = ljungbox(data, h)
-    % Assumes that data is mean centered
-    gamma = 1:h+1;
+    gamma = acvf(data, h);
     n = length(data);
-   
-    for i = 0:h
-        gamma(i + 1) = data(1 + i:end)' * data(1:end - i) / n;
-    end
     
     acf = gamma(2:end) / gamma(1);
     
